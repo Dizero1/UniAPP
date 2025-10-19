91a9f10db1975af9ae1dd9b9270595dec4126a6f
Dizero1 <dizero1662@163.com>
2025-10-19 22:12:05 +1100

    main menu

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import student
-import data_manager
-if __name__ == "__main__":
+from data_manager import Database
+import admin
+def menu():
     while True:
-        input_ = input("xxx")
-        if input_.lower() == 'x':
+        input_ = input("University System:(A)dmin, (S)tudent, or X:")
+        if input_.lower() == 'a':
+            db = Database()
+            # AdminController(db).menu()
+        elif input_.lower() == 's':
+            db = Database()
+            student.StudentController(db).menu()
+        elif input_.lower() == 'x':
+            print("Thank you")
             break
         else:
             print("Invalid input")
+
+if __name__ == "__main__":
+    menu()
